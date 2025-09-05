2e4bf24bf546dd016c66d8196dfb50a972d19ed1
Junayd Bhoyroo <contact@junaydb.com>
2025-09-05 16:11:07 +0100

    chore(backend): add auth dir

--- backend/src/main/auth/auth.ts
+++ backend/src/main/auth/auth.ts
@@ -5,6 +5,7 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import db from "../db/index.js";
 
+// TODO: add Discord
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
