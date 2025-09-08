435a8a7de65948c8298f1c9fa2b5e646062188b7
Junayd Bhoyroo <contact@junaydb.com>
2025-09-08 23:12:28 +0100

    (frontend): add auth and board providers

--- frontend/src/providers/AuthProvider.tsx
+++ frontend/src/providers/AuthProvider.tsx
@@ -1,7 +1,27 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { authClient } from "@/auth/auth-client";
 
-const AuthContext = createContext(null);
+type User = typeof authClient.signIn.social;
+type SetUser = (user: User) => void;
+
+const UserContext = createContext<User | null>(null);
+const SetUserContext = createContext<SetUser | null>(() => {});
 
 export function useAuth() {
-  return useContext(AuthContext);
+  return useContext(UserContext);
 }
+export function useSetAuth() {
+  return useContext(SetUserContext);
+}
+
+function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+
+  return (
+    <UserContext value={user}>
+      <SetUserContext value={setUser}>{children}</SetUserContext>
+    </UserContext>
+  );
+}
+
+export default AuthProvider;
