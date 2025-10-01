69228cbb7ee5c697e4de729618b8b54f1088c537
Junayd Bhoyroo <contact@junaydb.com>
2025-10-01 20:57:08 +0100

    chore(backend): format

--- backend/src/main/db/index.ts
+++ backend/src/main/db/index.ts
@@ -8,11 +8,8 @@ import { sql } from "drizzle-orm";
 let dbUrl =
   process.env.PROD === "true" ? process.env.DB_URL! : process.env.DB_URL_TEST!;
 
-const pool = new Pool({
-  connectionString: dbUrl,
-});
+const pool = new Pool({ connectionString: dbUrl });
 const db = drizzle({ client: pool });
-
 const res = await db.execute(sql`SELECT current_database()`);
 
 console.log("Connected to database: " + res.rows[0].current_database);
