f62855bbfe40ce1440397476a308c326207b240a
Junayd Bhoyroo <contact@junaydb.com>
2025-10-07 17:08:05 +0100

    chore(frontend): make betterauth baseURL dependent on env

--- frontend/src/auth/auth-client.ts
+++ frontend/src/auth/auth-client.ts
@@ -1,5 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:5173",
+  baseURL:
+    process.env.NODE_ENV === "prod"
+      ? process.env.BASE_URL
+      : "http://localhost:5173",
 });
