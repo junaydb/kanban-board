b676f022e9920d16307ee8f80bd87f5e1b01b757
Junayd Bhoyroo <contact@junaydb.com>
2025-10-23 00:18:12 +0100

    feat(frontend): disable create board button when max board limit reached

--- frontend/src/components/sidebar/SidebarBoards.tsx
+++ frontend/src/components/sidebar/SidebarBoards.tsx
@@ -6,6 +6,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/shadcn/ui/sidebar";
+import { Tooltip, TooltipTrigger, TooltipContent } from "@/shadcn/ui/tooltip";
 import Banner from "../Banners";
 import SpinnerBar from "../SpinnerBar";
 import CreateBoardFormDialog from "../CreateBoardFormDialog";
@@ -15,30 +16,49 @@ import { useGetAllBoards } from "@/trpc/board-hooks";
 function SidebarBoards() {
   const { data, error, isPending } = useGetAllBoards();
 
-  const boardData = data?.data.boards.map(({ id, title }) => ({ id, title }));
+  const canCreateBoards = data?.meta.boardCount < data?.meta.boardCountLimit;
 
-  // TODO:
-  // - disable create board button when the user is at max board count
+  const boardData = data?.data.boards.map(({ id, title }) => ({
+    id,
+    title,
+  }));
+
+  const createBoardButton = (
+    <SidebarMenuItem>
+      <SidebarMenuButton
+        className="flex justify-center border-2 border-dashed"
+        disabled={!canCreateBoards}
+      >
+        <Plus />
+      </SidebarMenuButton>
+    </SidebarMenuItem>
+  );
+
   return (
     <SidebarGroup>
       <SidebarGroupLabel>Boards</SidebarGroupLabel>
       <SidebarMenu>
         <CreateBoardFormDialog>
-          <SidebarMenuItem>
-            <SidebarMenuButton className="flex justify-center border-2 border-dashed">
-              <Plus />
-            </SidebarMenuButton>
-          </SidebarMenuItem>
+          {canCreateBoards ? (
+            createBoardButton
+          ) : (
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <span className="inline-block w-full cursor-pointer">
+                  {createBoardButton}
+                </span>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Maximum board limit reached</p>
+              </TooltipContent>
+            </Tooltip>
+          )}
         </CreateBoardFormDialog>
         {isPending ? (
           <SpinnerBar>Loading boards...</SpinnerBar>
         ) : error?.data?.code === "UNAUTHORIZED" ? (
           <SidebarMenuItem>
             <Banner banner="AUTHORISATION_ERROR" />
-          </SidebarMenuItem>
-        ) : !data?.success ? (
-          <SidebarMenuItem>
-            <Banner banner="BOARD_FETCH_ERROR" />
           </SidebarMenuItem>
         ) : (
           boardData?.map(({ id, title }) => (
