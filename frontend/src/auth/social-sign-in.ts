fe41448dc60d7c61f8cc461159eea97a0a7b762e
Junayd Bhoyroo <contact@junaydb.com>
2026-01-02 15:56:42 +0000

    fix(frontend): fix broken callback url

--- frontend/src/auth/social-sign-in.ts
+++ frontend/src/auth/social-sign-in.ts
@@ -1,10 +1,10 @@
 import { authClient } from "./auth-client";
 import type { AuthProvider } from "@/util/auth-providers";
 
-function getUrlPrefix(path = "/") {
+function getUrlPrefix(path?: string) {
   return process.env.NODE_ENV === "prod"
     ? path
-    : `http://localhost:5173/${path}`;
+    : `http://localhost:5173/${path ?? ""}`;
 }
 
 export async function socialSignIn(provider: AuthProvider) {
