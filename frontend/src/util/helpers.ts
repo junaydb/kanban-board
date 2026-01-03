87476111f950186e4e9b9e5237dbd6b153bad202
Junayd Bhoyroo <contact@junaydb.com>
2026-01-03 01:08:47 +0000

    refactor(frontend): remove redundant fn

--- frontend/src/util/helpers.ts
+++ frontend/src/util/helpers.ts
@@ -1,22 +1,8 @@
 import type { TaskStatusEnum } from "@backend/util/types";
 
-export function statusEnumToDisplay(status: TaskStatusEnum) {
-  const statusMapper = {
-    TODO: "To do",
-    IN_PROGRESS: "In progress",
-    DONE: "Done",
-  };
-
-  return statusMapper[status];
-}
-
 export function getSetStatusOptions(status: TaskStatusEnum) {
   const statusArr: TaskStatusEnum[] = ["TODO", "IN_PROGRESS", "DONE"];
   return statusArr.filter((cur) => status !== cur);
-}
-
-export function combineDateAndTimeToISO(date: string, time: string): string {
-  return new Date(`${date}T${time}`).toISOString();
 }
 
 export function toLowerKebabCase(str = "") {
